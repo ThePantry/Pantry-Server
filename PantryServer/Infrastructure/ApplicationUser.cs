91c0f943a512105a15a3426bd07a0caf1d994515
Ashley <Ashley>
2016-03-08 19:48:57 +1300

    Updated all packages starting on part 3

--- PantryServer/Infrastructure/ApplicationUser.cs
+++ PantryServer/Infrastructure/ApplicationUser.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace PantryServer.Infrastructure
@@ -22,5 +25,12 @@ namespace PantryServer.Infrastructure
 
         [Required]
         public DateTime JoinDate { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
     }
 }
