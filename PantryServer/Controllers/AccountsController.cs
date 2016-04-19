1959593f9d45e37fcdc05dfd22ded3980af44c1b
Dan <dan.lewis@devacademy.co.nz>
2016-04-19 21:50:10 +1200

    remove requirements of profile properties

--- PantryServer/Controllers/AccountsController.cs
+++ PantryServer/Controllers/AccountsController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 using Microsoft.AspNet.Identity;
 using PantryServer.Infrastructure;
@@ -22,18 +21,18 @@ namespace PantryServer.Controllers
         [System.Web.Mvc.Route("users")]
         public IHttpActionResult GetUsers()
         {
-            return Ok(this.AppUserManager.Users.ToList().Select(u => this.TheModelFactory.Create(u)));
+            return Ok(AppUserManager.Users.ToList().Select(u => TheModelFactory.Create(u)));
         }
 
         [Authorize(Roles = "Admin")]
         [System.Web.Mvc.Route("user/{id:guid}", Name = "GetUserById")]
         public async Task<IHttpActionResult> GetUser(string Id)
         {
-            var user = await this.AppUserManager.FindByIdAsync(Id);
+            var user = await AppUserManager.FindByIdAsync(Id);
 
             if (user != null)
             {
-                return Ok(this.TheModelFactory.Create(user));
+                return Ok(TheModelFactory.Create(user));
             }
 
             return NotFound();
@@ -44,11 +43,11 @@ namespace PantryServer.Controllers
         [System.Web.Mvc.Route("user/{username}")]
         public async Task<IHttpActionResult> GetUserByName(string username)
         {
-            var user = await this.AppUserManager.FindByNameAsync(username);
+            var user = await AppUserManager.FindByNameAsync(username);
 
             if (user != null)
             {
-                return Ok(this.TheModelFactory.Create(user));
+                return Ok(TheModelFactory.Create(user));
             }
 
             return NotFound();
@@ -65,28 +64,28 @@ namespace PantryServer.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = new ApplicationUser()
+            var user = new ApplicationUser
             {
                 UserName = createUserModel.Username,
                 Email = createUserModel.Email,
                 FirstName = createUserModel.FirstName,
                 LastName = createUserModel.LastName,
                 Level = 3,
-                JoinDate = DateTime.Now.Date,
+                JoinDate = DateTime.Now.Date
             };
 
-            IdentityResult addUserResult = await this.AppUserManager.CreateAsync(user, createUserModel.Password);
+            IdentityResult addUserResult = await AppUserManager.CreateAsync(user, createUserModel.Password);
 
             if (!addUserResult.Succeeded)
             {
                 return GetErrorResult(addUserResult);
             }
 
-            string code = await this.AppUserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+            string code = await AppUserManager.GenerateEmailConfirmationTokenAsync(user.Id);
 
-            var callbackUrl = new Uri(Url.Link("ConfirmEmailRoute", new { userId = user.Id, code = code }));
+            var callbackUrl = new Uri(Url.Link("ConfirmEmailRoute", new { userId = user.Id, code }));
 
-            await this.AppUserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+            await AppUserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
             Uri locationHeader = new Uri(Url.Link("GetUserById", new { id = user.Id }));
 
@@ -105,16 +104,13 @@ namespace PantryServer.Controllers
                 return BadRequest(ModelState);
             }
 
-            IdentityResult result = await this.AppUserManager.ConfirmEmailAsync(userId, code);
+            IdentityResult result = await AppUserManager.ConfirmEmailAsync(userId, code);
 
             if (result.Succeeded)
             {
                 return Ok();
             }
-            else
-            {
-                return GetErrorResult(result);
-            }
+            return GetErrorResult(result);
         }
 
 
@@ -128,7 +124,7 @@ namespace PantryServer.Controllers
 
             IdentityResult result =
                 await
-                    this.AppUserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword,
+                    AppUserManager.ChangePasswordAsync(User.Identity.GetUserId(), model.OldPassword,
                         model.NewPassword);
 
             if (!result.Succeeded)
@@ -146,11 +142,11 @@ namespace PantryServer.Controllers
 
             //Only SuperAdmin or Admin can delete users (Later when implement roles)
 
-            var appUser = await this.AppUserManager.FindByIdAsync(id);
+            var appUser = await AppUserManager.FindByIdAsync(id);
 
             if (appUser != null)
             {
-                IdentityResult result = await this.AppUserManager.DeleteAsync(appUser);
+                IdentityResult result = await AppUserManager.DeleteAsync(appUser);
 
                 if (!result.Succeeded)
                 {
@@ -171,16 +167,16 @@ namespace PantryServer.Controllers
         public async Task<IHttpActionResult> AssignRolesToUser([FromUri] string id, [FromBody] string[] rolesToAssign)
         {
 
-            var appUser = await this.AppUserManager.FindByIdAsync(id);
+            var appUser = await AppUserManager.FindByIdAsync(id);
 
             if (appUser == null)
             {
                 return NotFound();
             }
 
-            var currentRoles = await this.AppUserManager.GetRolesAsync(appUser.Id);
+            var currentRoles = await AppUserManager.GetRolesAsync(appUser.Id);
 
-            var rolesNotExists = rolesToAssign.Except(this.AppRoleManager.Roles.Select(x => x.Name)).ToArray();
+            var rolesNotExists = rolesToAssign.Except(AppRoleManager.Roles.Select(x => x.Name)).ToArray();
 
             if (rolesNotExists.Count() > 0)
             {
@@ -189,7 +185,7 @@ namespace PantryServer.Controllers
                 return BadRequest(ModelState);
             }
 
-            IdentityResult removeResult = await this.AppUserManager.RemoveFromRolesAsync(appUser.Id, currentRoles.ToArray());
+            IdentityResult removeResult = await AppUserManager.RemoveFromRolesAsync(appUser.Id, currentRoles.ToArray());
 
             if (!removeResult.Succeeded)
             {
@@ -197,7 +193,7 @@ namespace PantryServer.Controllers
                 return BadRequest(ModelState);
             }
 
-            IdentityResult addResult = await this.AppUserManager.AddToRolesAsync(appUser.Id, rolesToAssign);
+            IdentityResult addResult = await AppUserManager.AddToRolesAsync(appUser.Id, rolesToAssign);
 
             if (!addResult.Succeeded)
             {
@@ -219,7 +215,7 @@ namespace PantryServer.Controllers
                 return BadRequest(ModelState);
             }
 
-            var appUser = await this.AppUserManager.FindByIdAsync(id);
+            var appUser = await AppUserManager.FindByIdAsync(id);
 
             if (appUser == null)
             {
@@ -231,10 +227,10 @@ namespace PantryServer.Controllers
                 if (appUser.Claims.Any(c => c.ClaimType == claimModel.Type))
                 {
 
-                    await this.AppUserManager.RemoveClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
+                    await AppUserManager.RemoveClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
                 }
 
-                await this.AppUserManager.AddClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
+                await AppUserManager.AddClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
             }
 
             return Ok();
@@ -251,7 +247,7 @@ namespace PantryServer.Controllers
                 return BadRequest(ModelState);
             }
 
-            var appUser = await this.AppUserManager.FindByIdAsync(id);
+            var appUser = await AppUserManager.FindByIdAsync(id);
 
             if (appUser == null)
             {
@@ -262,7 +258,7 @@ namespace PantryServer.Controllers
             {
                 if (appUser.Claims.Any(c => c.ClaimType == claimModel.Type))
                 {
-                    await this.AppUserManager.RemoveClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
+                    await AppUserManager.RemoveClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
                 }
             }
 
--- PantryServer/Models/AccountBindingModels.cs
+++ PantryServer/Models/AccountBindingModels.cs
@@ -18,15 +18,12 @@ namespace PantryServer.Models
             [Display(Name = "Email")]
             public string Email { get; set; }
 
-            [Required]
             [Display(Name = "Username")]
             public string Username { get; set; }
 
-            [Required]
             [Display(Name = "First Name")]
             public string FirstName { get; set; }
-
-            [Required]
+                                                                                                                                                                  
             [Display(Name = "Last Name")]
             public string LastName { get; set; }
 
