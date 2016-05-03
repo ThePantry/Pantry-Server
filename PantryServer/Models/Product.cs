dd88d5598177bdb8536559e12659a8a44e5cec94
Ashley <Ashley>
2016-05-03 19:39:03 +1200

    adding product updates

--- PantryServer/Models/Product.cs
+++ PantryServer/Models/Product.cs
@@ -6,6 +6,8 @@ namespace PantryServer.Models
     public class Product : IProduct
     {
         public int Id { get; set; }
+        //TODO move to inventry service
+        public int? Quantity { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Tag> Tags { get; set; } 
