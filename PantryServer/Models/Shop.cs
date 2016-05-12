ee717252434cbbf57f213c58c5382dfb6b4f772d
Dan <dan.lewis@devacademy.co.nz>
2016-05-12 19:33:06 +1200

    remove page shit

--- PantryServer/Models/Shop.cs
+++ PantryServer/Models/Shop.cs
@@ -10,7 +10,6 @@ namespace PantryServer.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Product> Products { get; set; }
-        public List<IPage> Pages { get; set; }
         public string Location { get; set; }
         public string Description { get; set; }
         public string Email { get; set; }
