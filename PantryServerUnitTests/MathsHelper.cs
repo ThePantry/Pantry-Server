f674d1089ceb274bf89c15b436011696d96905b6
Ashley Stewart <ashley.stewart@equinox.co.nz>
2016-06-16 09:41:48 +1200

    added NUnit and a fake test

---
+++ PantryServerUnitTests/MathsHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PantryServerUnitTests
+{
+    internal class MathsHelper
+    {
+        public MathsHelper() { }
+        public int Add(int a, int b)
+        {
+            int x = a + b;
+            return x;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            int x = a - b;
+            return x;
+        }
+    }
+}
